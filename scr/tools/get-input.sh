d14e80bc27d9d8f8091c4cbaadd5ff943d1bdeb4
Lux Brush <82244625+LuxBrush@users.noreply.github.com>
2024-11-11 21:23:03 -0800

    Updated get-input.sh
    Updated get-input.sh to be easier to read.

--- scr/tools/get-input.sh
+++ scr/tools/get-input.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# Function to read characters from a file, one by one, into an array
 get-input() {
-  local output=()
-  while read -n1 flor; do
-    output+=("$flor")
-  done <$1
-  echo "${output[@]}"
+  local characters=() # Array to store individual characters from the file
+
+  # Read each character from the specified file, one at a time
+  while read -n1 char; do
+    characters+=("$char") # Append each character to the array
+  done <"$1"              # Read from the file provided as the first argument
+
+  # Output all characters as a single space-separated string
+  echo "${characters[@]}"
 }
