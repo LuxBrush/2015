f8dc88a6e94327296e0052c4ec575c812afb3131
LuxBrush <82244625+LuxBrush@users.noreply.github.com>
2024-11-14 15:43:12 -0800

    Finshed Day 02, Part 01

--- scr/day02/day02.sh
+++ scr/day02/day02.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
 . ./tools/get-input.sh
+. ./tools/min.sh
 
 day02-part01() {
-  local output=0
-  local input=$1
+	local output=1586300
+	local input=$1
 
-  if [[ $input == "run" ]]; then
-    output=1
-  fi
+	if [[ $input == "run" ]]; then
+		output=0
+		local measurements=$(get-input "line" "./day02/input.txt")
+		for m in $measurements; do
+			IFS='x' read -r l w h <<<"$m"
 
-  printf "Day 02, Part 01: %s" "${output}"
+			local side01=$((l * w))
+			local side02=$((w * h))
+			local side03=$((h * l))
+			local small_side=$(min $side01 $side02 $side03)
+
+			local square_feet=$((2 * side01 + 2 * side02 + 2 * side03 + small_side))
+
+			((output += $square_feet))
+		done
+	fi
+
+	printf "Day 02, Part 01: %s" "${output}"
 }
